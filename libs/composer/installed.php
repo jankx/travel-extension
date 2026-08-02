e8fcb75b2d5a9c8b2f786e67ca0dfe60812342ef
Puleeno Nguyen <puleeno@gmail.com>
2026-08-02 22:33:58 +0700

    feat: add Composer autoloader support to the travel extension library

---
+++ libs/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jankx/travel-extension',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '86c016f1f927c86216af70104f55dfbe6dece5b8',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jankx/travel-extension' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '86c016f1f927c86216af70104f55dfbe6dece5b8',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
