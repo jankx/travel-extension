50c781a8951fecd45fa8f432647dc22295038f0f
Puleeno Nguyen <puleeno@gmail.com>
2026-09-15 20:37:53 +0700

    refactor: remove unused CSS and parent block reference from Post Starting Price block

--- blocks/post-starting-price/build/index.tsx.asset.php
+++ blocks/post-starting-price/build/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '40ae8697efafdc21cc6d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '4fb09bdba34455ffab1f');
